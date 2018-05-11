2ae1fb31bb09883980e22401258660ed69bda6f3
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-11 10:49:10 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+function forLoop(array) {
+  for(let i=1; i <= 25; i++) {
+    
+    if(i<=0) {
+    array.unshift('I am a strange loop.');
+    }
+    else {
+      array.unshift('I am ${i} strange loops.')
+    }
+    
+    return array;
+  
+}}
